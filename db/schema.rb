c6fe2279ec7be141ec7288b515922381cbc60d3f
YOUR NAME <solisamo6@gmail.com>
2019-09-16 21:44:16 -0500

    Se creo la tabla task con el campo task name y body para poder añadir comentarios a las listas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_015443) do
+ActiveRecord::Schema.define(version: 2019_09_17_021949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_09_17_015443) do
     t.string "list_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task_name"
+    t.string "body"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_09_17_015443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "lists"
 end
