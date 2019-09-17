3e5fa96146937a35f38aa8f4e9a9c5bd157c82fe
YOUR NAME <solisamo6@gmail.com>
2019-09-17 00:00:18 -0500

    Se creo la migracion donde list hace referencia a users para poder hacer independientes las listas a el usuario que las crea haciendo user_id la llave foranea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_021949) do
+ActiveRecord::Schema.define(version: 2019_09_17_044357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_09_17_021949) do
     t.string "list_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 2019_09_17_021949) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lists", "users"
   add_foreign_key "tasks", "lists"
 end
