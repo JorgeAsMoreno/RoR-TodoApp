fbafa8b6b34195d9c806aa72d87825af5d8d5dd3
YOUR NAME <solisamo6@gmail.com>
2019-09-16 21:06:22 -0500

    Se crea la tabla list y se hace la migracion para poder alamcenar el list name

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_012017) do
+ActiveRecord::Schema.define(version: 2019_09_17_015443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "list_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
